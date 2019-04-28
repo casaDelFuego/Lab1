42732bd5d4fa5d348ce3caea9e2fff61aa5795d0
Olga <tselyuk.olga@gmail.com>
2019-04-29 00:00:27 +0200

    account service has been created

--- src/app/bank/account.service.spec.ts
+++ src/app/bank/account.service.spec.ts
@@ -4,11 +4,29 @@ import { AccountService } from './account.service';
 
 describe('AccountService', () => {
   let service: AccountService;
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(AccountService);
+  });
 
   it('should be created', () => {
     const service: AccountService = TestBed.get(AccountService);
     expect(service).toBeTruthy();
   });
+
+
+  it ('should have 0 or positive balance', () => {
+    let balance = 100;
+    let actual = service.isBalancePositive(balance);
+    expect(actual).toBe(true);
+  });
+
+   it ('should have a name of a customer', ()=> {
+     let name = "random dude";
+     let actual = service.isNameValid(name);
+     expect(actual).toBe(true);
+   });
+
+   
 
 });
