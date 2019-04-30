c8e4f8757d01a8278723649ced6fcc3bc5db6b4f
Olga <tselyuk.olga@gmail.com>
2019-04-30 23:52:48 +0200

    bank component has a name

--- src/app/bank/bank.component.spec.ts
+++ src/app/bank/bank.component.spec.ts
@@ -1,14 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { BankComponent } from './bank.component';
+import { BankService } from './bank.service';
+import { Account} from './../account';
 
 describe('BankComponent', () => {
   let component: BankComponent;
   let fixture: ComponentFixture<BankComponent>;
+  let domElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BankComponent ]
+      declarations: [ BankComponent ],
+      imports: [FormsModule],
+      providers: [BankService]
     })
     .compileComponents();
   }));
@@ -16,6 +21,7 @@ describe('BankComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BankComponent);
     component = fixture.componentInstance;
+    domElement = fixture.nativeElement;
     fixture.detectChanges();
 
   });
@@ -24,8 +30,20 @@ describe('BankComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it(`should have as title 'Bank'`, () => {
+		const fixture = TestBed.createComponent(BankComponent);
+		const bank = fixture.debugElement.componentInstance;
+		expect(bank.title).toEqual('Bank');
+	});
 
-
+  // it('should be able to show an account on the page', () => {
+  //   let account: Account = {
+  //     customerName: 'jon',
+  //     balance: 100
+  //   };
+  //   let accountInfo = element.querySelector
+  //   expect(account).toBeTruthy();
+  // });
 
 
 });
